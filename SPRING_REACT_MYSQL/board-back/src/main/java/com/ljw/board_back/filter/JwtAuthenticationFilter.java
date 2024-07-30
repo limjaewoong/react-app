cae03b29387f1df8e66f733f705b42ad16de88c4
iju <ijujjang6@gmail.com>
2024-07-30 17:09:54 +0900

    07.29 commit

---
+++ SPRING_REACT_MYSQL/board-back/src/main/java/com/ljw/board_back/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,40 @@
+package com.ljw.board_back.filter;
+
+import java.io.IOException;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+import com.ljw.board_back.provider.Jwtprovider;
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter{
+    
+    private final Jwtprovider jwtprovider;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        throw new UnsupportedOperationException("Unimplemented method 'doFilterInternal'");
+    }
+
+    @SuppressWarnings("unused")
+    private String parseBearerToken(HttpServletRequest request){
+        String authorization = request.getHeader("Authorization");
+
+        boolean hasAuthorization = StringUtils.hasText(authorization);
+        if(!hasAuthorization) return null;
+
+        boolean isBearer = authorization.startsWith("Bearer ");
+        if(!isBearer) return null;
+
+        String token = authorization.substring(7);
+        return token;
+    }
+
+}
---
+++ SPRING_REACT_MYSQL/board-back/src/main/java/com/ljw/board_back/provider/Jwtprovider.java
@@ -0,0 +1,44 @@
+package com.ljw.board_back.provider;
+
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+@Component
+public class Jwtprovider {
+    
+    private String secretKey = "S3cr3tk3y";
+
+    public String create(String email){
+        Date expireDate = Date.from(Instant.now().plus(1,ChronoUnit.HOURS));
+
+        String jwt = Jwts.builder()
+            .signWith(SignatureAlgorithm.ES256, secretKey)
+            .setSubject(email).setIssuedAt(new Date()).setExpiration(expireDate)
+            .compact();
+
+        return jwt;
+    }
+
+    @SuppressWarnings({ "null"})
+    public String validate(String jwt){
+
+        Claims claims = null;
+
+        try {
+            claims = Jwts.parser().setSigningKey(secretKey).parseClaimsJwt(jwt).getBody();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return claims.getSubject();
+    }
+}
